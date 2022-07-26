992e390eb85377dfbf3ff57f173e6c6c87e3921a
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-07-26 11:20:21 -0300

    test: create Main components tests

---
+++ src/components/Main/index.spec.tsx
@@ -0,0 +1,16 @@
+import { render } from "@testing-library/react";
+import Main from ".";
+
+describe("Main Component", () => {
+  it("should have a main tag", () => {
+    const children: JSX.Element = <span>Main children</span>;
+
+    const { container, debug } = render(<Main>{children}</Main>);
+
+    const mainElement: HTMLElement | null = container.querySelector("main");
+
+    expect(mainElement).toBeInTheDocument();
+
+    debug();
+  });
+});
