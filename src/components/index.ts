0d112cefb6dcc9b9af2cb2c502edaa02d860f5bc
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-07-27 12:10:28 -0300

    feat: create JobsArea component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,21 @@
 import AboutUsArea from "./AboutUsArea";
 import Header from "./Header";
+import JobOpportunity from "./JobOpportunity";
+import JobPost from "./JobPost";
+import JobsArea from "./JobsArea";
 import Main from "./Main";
 import MenuItem from "./MenuItem";
 import MetricItem from "./MetricItem";
 import Navbar from "./Navbar";
 
-export { AboutUsArea, Header, MetricItem, Main, MenuItem, Navbar };
+export {
+  AboutUsArea,
+  Header,
+  JobOpportunity,
+  JobPost,
+  JobsArea,
+  Main,
+  MenuItem,
+  MetricItem,
+  Navbar,
+};
